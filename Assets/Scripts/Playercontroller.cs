990d3ebc64913244ac8c26c7ce907fb4868a84a4
Helen <31750014+hxgreensoul@users.noreply.github.com>
2021-02-07 16:39:09 -0800

    Work for Unit 1 Lesson B

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.deltaTime);
+        //vector forward --> (0,0,1)
+        //moves car forward, over each frame at a speed of 0.16 
+        transform.Translate(Vector3.forward * Time.deltaTime *10);
+    }
+}
