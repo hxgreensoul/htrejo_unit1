3868a3974744fd78917a6eaa913a4be93de47f55
Helen <31750014+hxgreensoul@users.noreply.github.com>
2021-02-09 20:19:45 -0800

    Modified Playercontroller to turn and rotate

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
-    public float speed = 15.0f; //decimal value for speed
+    private float speed = 15.0f; //decimal value for speed
+    private float turnSpeed = 15.0f; //decimal value for turning speed
+
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +19,11 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.deltaTime);
-        //vector forward --> (0,0,1)
-        //moves car forward, over each frame at a speed of 0.16 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal"); // assigns variable to go side to side
+        verticalInput = Input.GetAxis("Vertical"); // assigns variable to rotate
+
+        Debug.Log(Time.deltaTime); 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput); //makes car rotate
     }
 }
